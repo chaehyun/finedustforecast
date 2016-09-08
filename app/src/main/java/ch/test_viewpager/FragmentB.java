de669e08381dc0b36a9230a83a51eae9e2d3265f
sihon321 <sihon321@gmail.com>
2016-09-08 17:32:12 +0900

    test branch commit

--- app/src/main/java/ch/test_viewpager/FragmentB.java
+++ app/src/main/java/ch/test_viewpager/FragmentB.java
@@ -31,7 +31,6 @@ public class FragmentB extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
